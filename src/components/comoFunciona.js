b0f9e7344f87a0773c48b5a1ca3db503e8d2b660
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-05 19:01:47 -0300

    accordion add

--- src/components/comoFunciona.js
+++ src/components/comoFunciona.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { IoIosArrowDown } from "react-icons/io";
+import $ from "jquery"
+import { useEffect } from "react";
 
 
 function ComoFunciona() {
+useEffect(() => {
+   $('.accord1 li p').click(function(){
+    $(this).next(".accord-content").slideToggle();
+});
+  })
   return(
-    <div className='p-3 bg-neutral-700'>
-        <h1 className="text-center text-neutral-50 text-xl font-semibold">Como funciona ?</h1>
-        <div className='faqItem'>
-            <p>1. Escolha seu plano</p>
-            <p className='faqHiden'></p>
+    <div className='p-6 bg-neutral-800'>
+        <div className='container mx-auto'>
+        <div className="accord1">
+    <ul>
+      <li>
+        <p>Accord 1</p>
+        <div className="accord-content">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+        </div>
+      </li>
+      <li>
+        <p>Accord 2</p>
+        <div className="accord-content">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+        </div>
+      </li>
+      <li>
+        <p>Accord 3</p>
+        <div className="accord-content">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+        </div>
+      </li>
+      
+    </ul>
+  </div>
         </div>
     </div>
   );
--- src/components/site redirect.js
+++ src/components/site redirect.js
@@ -3,7 +3,7 @@ import { MdShoppingBag } from "react-icons/md";
 
 function Redirect() {
   return(
-    <div className='text-center p-3 text-lg bg-white'>
+    <div className='text-center p-8 text-lg bg-white'>
       <div className='flex items-center justify-center text-neutral-800 font-medium'><p>Você também pode ver todos os produtos na nossa <a href='https://bellahair.com.br' target='blank' className='button items-center justify-center flex py-0.5 px-2 rounded w-20 mx-auto'><MdShoppingBag className='mr-0.5' />Loja</a></p></div>
     </div>
   );
