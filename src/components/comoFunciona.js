e92dfef322ab910e43366f9c7d8e7fd858bc25f0
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-05 19:50:24 -0300

    accordion fix

--- src/components/comoFunciona.js
+++ src/components/comoFunciona.js
@@ -7,9 +7,11 @@ import { useEffect } from "react";
 function ComoFunciona() {
 useEffect(() => {
     $( ".faqItem" ).on('click', function() {
-        $(this).next("p").slideToggle('slow');
+        $(this).next("p").slideToggle('1000');
+        $(this).next(".faqIcon").toggleClass('active');
       }
     );
+
     $(document).ready(function() {
         $(".faq").children("p").hide();
       })
@@ -21,17 +23,17 @@ useEffect(() => {
             <div className='faq text-neutral-50 drop-shadow-xl bg-rose-400 rounded text-xl overflow-hidden relative my-3'>
                 <h3 className='faqItem text-2xl cursor-pointer p-4'>1. Escolha seu plano</h3>
                 <p className='faqHidden px-4 pb-2'>Quando clicar no botão de assinar, irá para a página de <a href='#' className='text-rose-800 font-semibold'>registro</a>, onde criará sua conta e dará continuidade à contratação do plano.</p>
-                <button className='faqIcon absolute right-3 top-1/2 text-rose-200' style={{transform: 'translate(0, -50%)'}}><IoIosArrowDown size={26}/></button>
+                <div className='faqIcon absolute right-3 top-5 text-rose-200'><IoIosArrowDown size={26}/></div>
             </div>
             <div className='faq text-neutral-50 drop-shadow-xl bg-rose-400 rounded text-xl overflow-hidden relative my-3'>
                 <h3 className='faqItem text-2xl cursor-pointer p-4'>2. Seleção de preferências</h3>
                 <p className='faqHidden px-4 pb-2'>Ao adquirir o plano, você selecionará as preferências do produto <span className='text-rose-800 font-semibold'>(loiro, castanho, curto, longo, etc)</span>.</p>
-                <button className='faqIcon absolute right-3 top-1/2 text-rose-200' style={{transform: 'translate(0, -50%)'}}><IoIosArrowDown size={26}/></button>
+                <div className='faqIcon absolute right-3 top-5 text-rose-200'><IoIosArrowDown size={26}/></div>
             </div>
             <div className='faq text-neutral-50 drop-shadow-xl bg-rose-400 rounded text-xl overflow-hidden relative my-3'>
                 <h3 className='faqItem text-2xl cursor-pointer p-4'>3. Aproveite</h3>
                 <p className='faqHidden px-4 pb-2'>Todo mês você receberá um produto  <span className='text-rose-800 font-semibold'>selecionado</span> pra você.</p>
-                <button className='faqIcon absolute right-3 top-1/2 text-rose-200' style={{transform: 'translate(0, -50%)'}}><IoIosArrowDown size={26}/></button>
+                <div className='faqIcon absolute right-3 top-5 text-rose-200'><IoIosArrowDown size={26}/></div>
             </div>
         </div>
     </div>
