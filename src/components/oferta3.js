7c0a2ab0318c4f16f85c7c2190dd951e46f28154
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-05 16:23:51 -0300

    padding changed, bg-color changed

--- src/components/oferta3.js
+++ src/components/oferta3.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function oferta3() {
   return(
-    <div className='px-5 pb-7 pt-7 bg-neutral-800'>
+    <div className='px-5 py-16 bg-neutral-800'>
       <div className='container mx-auto'>
         <h1 className='uppercase text-3xl md:text-2xl text-white drop-shadow-sm tracking-tight text-center font-bold pb-2'>O <span style={{color: 'var(--secondary)'}}>ÚNICO</span> CLUBE DE <span style={{color: 'var(--primary)'}}>ASSINATURA</span> DE BELEZA DO <span style={{color: '#009b3a'}}>BRASIL</span></h1>
         <p className='text-neutral-100'>O <span style={{color: 'var(--secondary)'}}>Clube da Bella</span>, lançado em 2023, é uma iniciativa inovadora que reflete o compromisso contínuo da Bella Hair com o cliente. Como membro do Clube da Bella, os assinantes têm acesso a uma variedade de benefícios exclusivos, além de uma renovação visual mensal da cliente.</p>
