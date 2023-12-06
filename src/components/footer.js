c3c7364b0e091cac901ed8ae55e0edde146aae4d
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-05 21:05:26 -0300

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return(
-    <div className='flex justify-center items-center py-2 text-white' style={{backgroundColor: 'var(--primary-dark)'}}>
+    <div className='flex justify-center items-center py-2 text-white flex-wrap' style={{backgroundColor: 'var(--primary-dark)'}}>
         Feito por Nicolas Sposito, em parceria com a <img className='ml-1.5' src='https://acdn.mitiendanube.com/stores/001/608/438/themes/common/logo-689021723-1683587536-f9e019574a09d3122aff56a5d96c12881683587537-320-0.webp' width={100}></img>
     </div>
   );
