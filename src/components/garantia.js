b33ea7fac9aceddc1ebf91fd154ad4bde816e44f
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-06 01:57:32 -0300

    garantia spacing

--- src/components/garantia.js
+++ src/components/garantia.js
@@ -4,7 +4,7 @@ import logo2 from '../img/logo2.png'
 function Garantia() {
   return(
     <div>
-        <div className='container mx-auto py-6 text-center'>
+        <div className='container mx-auto py-6 text-center px-2'>
             <img src={logo2} width={300} className='mx-auto'></img>
             <h1 className='text-xl py-1 underline'>Garantia de Satisfação</h1>
             <p>Se você não ficar 100% satisfeito com a sua compra, entre em <a href='#contato' className='text-rose-400 font-semibold'>contato</a> conosco.</p>
