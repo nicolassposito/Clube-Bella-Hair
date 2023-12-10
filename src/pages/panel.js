f2b1d65e33049d28264d9331426a5146dce83e4a
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-10 20:43:11 -0300

    tippyjs

--- src/pages/panel.js
+++ src/pages/panel.js
@@ -12,20 +12,40 @@ function Panel() {
   tippy('.painel-item', {
     animation: 'shift-away',
     placement: 'right',
+    touch: 'boolean',
   });
+
+  let tippyInstances = tippy('[data-tippy-content]');
+
+  function updateTippy() {
+      if (window.innerWidth > 768) {
+          tippyInstances.forEach(instance => {
+              instance.disable();
+          });
+      } else {
+          tippyInstances.forEach(instance => {
+              instance.enable();
+          });
+      }
+  }
+
+  window.addEventListener('resize', updateTippy);
+
+  updateTippy();
+
   return(
     <div>
       <div className='p-4 border-r inline-block h-screen'>
         <div className='border-b pb-6'>
           <img src={logo} width={220} className='hidden md:block'></img>
-          <img src={logoB} width={120} className='md:hidden pl-3'></img>
+          <img src={logoB} width={70} className='md:hidden pl-2.5'></img>
         </div>
-        <ul className='py-4 text-lg'>
-          <li id="painel" data-tippy-content="Painel" className='painel-item flex items-center py-4 cursor-pointer text-slate-700 active'><MdSpaceDashboard className='mr-3'/>Painel</li>
-          <li id="assinatura" data-tippy-content="Assinatura" className='painel-item flex items-center py-4 cursor-pointer text-slate-700'><MdAutoAwesome className='mr-3'/>Assinatura</li>
-          <li id="preferencias" data-tippy-content="Preferências" className='painel-item flex items-center py-4 cursor-pointer text-slate-700'><MdAddReaction className='mr-3'/>Preferências</li>
-          <li id="solicitartroca" data-tippy-content="Solicitar troca" className='painel-item flex items-center py-4 cursor-pointer text-slate-700'><MdOutlineChangeCircle className='mr-3'/>Solicitar troca</li>
-          <li id="contato" data-tippy-content="Contato" className='painel-item flex items-center py-4 cursor-pointer text-slate-700'><MdContactSupport className='mr-3'/>Contato</li>
+        <ul className='py-4 text-xl md:text-lg text-center flex flex-col'>
+          <li id="painel" data-tippy-content="Painelaa" className='painel-item inline-flex md:flex justify-center md:justify-start items-center p-2 md:py-4 rounded-full md:rounded-0 cursor-pointer bg-slate-100 md:bg-transparent text-slate-700 active'><MdSpaceDashboard className='md:mr-3'/><span className='hidden md:block'>Painel</span></li>
+          <li id="assinatura" data-tippy-content="Assinatura" className='painel-item inline-flex md:flex justify-center md:justify-start items-center p-2 md:py-4 rounded-full md:rounded-0 cursor-pointer bg-slate-100 md:bg-transparent text-slate-700'><MdAutoAwesome className='md:mr-3'/><span className='hidden md:block'>Assinatura</span></li>
+          <li id="preferencias" data-tippy-content="Preferências" className='painel-item inline-flex md:flex justify-center md:justify-start items-center p-2 md:py-4 rounded-full md:rounded-0 cursor-pointer bg-slate-100 md:bg-transparent text-slate-700'><MdAddReaction className='md:mr-3'/><span className='hidden md:block'>Preferências</span></li>
+          <li id="solicitartroca" data-tippy-content="Solicitar troca" className='painel-item inline-flex md:flex justify-center md:justify-start items-center p-2 md:py-4 rounded-full md:rounded-0 cursor-pointer bg-slate-100 md:bg-transparent text-slate-700'><MdOutlineChangeCircle className='md:mr-3'/><span className='hidden md:block'>Solicitar troca</span></li>
+          <li id="contato" data-tippy-content="Contato" className='painel-item inline-flex md:flex justify-center md:justify-start items-center p-2 md:py-4 rounded-full md:rounded-0 cursor-pointer bg-slate-100 md:bg-transparent text-slate-700'><MdContactSupport className='md:mr-3'/><span className='hidden md:block'>Contato</span></li>
         </ul>
       </div>
     </div>
