d6e6d711ddb4e3d05a3409b32e31f7272385395d
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-10 19:58:12 -0300

    "

--- src/pages/panel.js
+++ src/pages/panel.js
@@ -4,10 +4,12 @@ import logoB from '../img/logoB.png';
 import { MdSpaceDashboard, MdAddReaction, MdAutoAwesome, MdOutlineChangeCircle, MdContactSupport } from "react-icons/md";
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
+import 'tippy.js/animations/shift-away.css';
 
 function Panel() {
-  tippy('#painel', {
-    content: 'My tooltip!',
+  tippy('.painel-item', {
+    animation: 'shift-away',
   });
   return(
     <div>
@@ -17,11 +19,11 @@ function Panel() {
           <img src={logoB} width={120} className='md:hidden pl-3'></img>
         </div>
         <ul className='py-4'>
-          <li id="painel" className='flex items-center'><MdSpaceDashboard className='mr-1'/>Painel</li>
-          <li id="assinatura" className='flex items-center'><MdAutoAwesome className='mr-1'/>Assinatura</li>
-          <li id="preferencias" className='flex items-center'><MdAddReaction className='mr-1'/>Preferências</li>
-          <li id="solicitartroca" className='flex items-center'><MdOutlineChangeCircle className='mr-1'/>Solicitar troca</li>
-          <li id="contato" className='flex items-center'><MdContactSupport className='mr-1'/>Contato</li>
+          <li id="painel" data-tippy-content="Painel" className='painel-item flex items-center py-4 cursor-pointer'><MdSpaceDashboard className='mr-3'/>Painel</li>
+          <li id="assinatura" data-tippy-content="Assinatura" className='painel-item flex items-center py-4 cursor-pointer'><MdAutoAwesome className='mr-3'/>Assinatura</li>
+          <li id="preferencias" data-tippy-content="Preferências" className='painel-item flex items-center py-4 cursor-pointer'><MdAddReaction className='mr-3'/>Preferências</li>
+          <li id="solicitartroca" data-tippy-content="Solicitar troca" className='painel-item flex items-center py-4 cursor-pointer'><MdOutlineChangeCircle className='mr-3'/>Solicitar troca</li>
+          <li id="contato" data-tippy-content="Contato" className='painel-item flex items-center py-4 cursor-pointer'><MdContactSupport className='mr-3'/>Contato</li>
         </ul>
       </div>
     </div>
