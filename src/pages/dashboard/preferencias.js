068a6731ddae4c27866a95ac50fed26cd101156d
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-17 20:22:32 -0300

    preferences

--- src/pages/dashboard/preferencias.js
+++ src/pages/dashboard/preferencias.js
@@ -14,12 +14,35 @@ function Preferences() {
           <p className='font-light text-sm'>Sertifique-se de escolher as opções que mais te agradam 😁.</p>
           <div>
             <ul className='text-center'>
-              <li className='flex items-center'>
+            <li className='flex items-center my-2 justify-center'>
                 <span className='mr-2'>Cor:</span>
-                <div className='flex'>
-                  <button className='bg-neutral-200 px-2 py-1 rounded'>Loiro</button>
+                <div className='preferencias flex'>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded active'>Loiro</button>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded'>Castanho</button>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded'>Preto</button>
                 </div>
               </li>
+              <li className='flex items-center my-2 justify-center'>
+                <span className='mr-2'>Tipo:</span>
+                <div className='preferencias flex'>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded'>Ondulado</button>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded'>Liso</button>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded'>Afro</button>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded active'>Cacheado</button>
+                </div>
+              </li>
+              <li className='flex items-center my-2 justify-center'>
+                <span className='mr-2'>Tamanho:</span>
+                <div className='preferencias flex'>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded'>50cm</button>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded active'>60cm</button>
+                  <button className='bg-neutral-200 px-2 mx-0.5 py-1 rounded'>70cm</button>
+                </div>
+              </li>
+              <li>
+                <textarea rows="3" cols="28" wrap="hard" maxlength="80" className='p-2 mt-2 outline outline-2 outline-offset-2 outline-pink-500 rounded' placeholder='Detalhe (opcional)'></textarea>
+              </li>
+              <button className='button py-2 px-6 inline-block mt-2 rounded'>Salvar</button>
             </ul>
           </div>
         </div>
