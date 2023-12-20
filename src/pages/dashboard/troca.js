2724e1b7afed311589f5c81d181ce86db0e9e0be
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-20 15:07:34 -0300

    spacing

--- src/pages/dashboard/troca.js
+++ src/pages/dashboard/troca.js
@@ -9,11 +9,12 @@ function SolicitarTroca() {
     </div>
     <div className='relative w-full'>
         <div className='mt-20'>
-            <h1 className='font-medium text-center text-2xl'>Solicitar troca de produto</h1>
-            <form className='flex flex-col items-center text-left max-w-md mx-auto mt-10'>
-                <input className='w-full outline outline-1 outline-pink-400 p-2 rounded my-1' placeholder='Nome'></input>
-                <input className='w-full outline outline-1 outline-pink-400 p-2 rounded my-1' type="number" placeholder='Telefone (DDD + Número)'></input>
-                <textarea rows="3" wrap="hard" maxLength="80" className='w-full p-2 mt-2 outline outline-1 outline-offset-2 outline-pink-500 rounded w-full' placeholder='Detalhes'></textarea>
+            <h1 className='font-medium text-center text-2xl text-neutral-700'>Solicitar troca de produto</h1>
+            <p className='text-center font-light text-neutral-700'>Insira suas informações abaixo que te contactaremos</p>
+            <form className='flex flex-col items-center text-left max-w-md mx-auto mt-10 px-2'>
+                <input className='w-full outline outline-1 outline-pink-300 p-2 rounded my-1' placeholder='Nome'></input>
+                <input className='w-full outline outline-1 outline-pink-300 p-2 rounded my-1' type="number" placeholder='Telefone (DDD + Número)'></input>
+                <textarea rows="3" wrap="hard" maxLength="80" className='w-full p-2 mt-2 outline outline-1 outline-offset-2 outline-pink-300 rounded w-full' placeholder='Detalhes'></textarea>
                 <button className='button py-2 px-6 inline-block mt-3 rounded' type='submit'>Enviar</button>
             </form>
         </div>
