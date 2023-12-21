53c3b63865df28f82d211b8e88d450742b3f30d8
Nicolas Sposito <noni.sposito@gmail.com>
2023-12-21 16:59:23 -0300

    homeDashboard

--- src/pages/dashboard/home.js
+++ src/pages/dashboard/home.js
@@ -3,6 +3,8 @@ import Panel from '../panel';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import "firebase/auth";
+import { MdSpaceDashboard, MdAddReaction, MdAutoAwesome, MdOutlineChangeCircle, MdContactSupport, MdOutlineLogout, MdAdd } from "react-icons/md";
+import { Link } from 'react-router-dom';
 
 function HomePainel() {
   const user = firebase.auth().currentUser;
@@ -12,8 +14,17 @@ function HomePainel() {
         <Panel />
     </div>
     <div className='relative w-full'>
-        <div className='mt-20'>
-          <h1>Bem vindo(a) {user.displayName}</h1>
+        <div className='mt-20 text-center'>
+          <h1 className='font-medium text-3xl'>Bem vindo(a) {user.displayName}</h1>
+          <div className='max-w-3xl mx-auto'>
+            <div className='flex flex-col md:grid grid-rows-2 grid-flow-col gap-3 mt-20 mx-4'>
+              <Link to="/painel/assinatura" className='inline-flex items-center shadow-lg outline outline-1 outline-rose-300 p-4 rounded text-rose-400 opacity-95 hover:opacity-100 hover:bg-rose-300 hover:shadow-xl hover:pointer hover:text-white transition'><MdAutoAwesome className='mr-1' /> Assinatura</Link>
+              <Link to="/painel/preferencias" className='inline-flex items-center shadow-lg outline outline-1 outline-rose-300 p-4 rounded text-rose-400 opacity-95 hover:opacity-100 hover:bg-rose-300 hover:shadow-xl hover:pointer hover:text-white transition'><MdAddReaction className='mr-1' /> Preferências</Link>
+              <Link to="/painel/troca" className='inline-flex items-center shadow-lg outline outline-1 outline-rose-300 p-4 rounded text-rose-400 opacity-95 hover:opacity-100 hover:bg-rose-300 hover:shadow-xl hover:pointer hover:text-white transition'><MdOutlineChangeCircle className='mr-1' /> Solicitar troca</Link>
+              <Link to="/painel/contato" className='inline-flex items-center shadow-lg outline outline-1 outline-rose-300 p-4 rounded text-rose-400 opacity-95 hover:opacity-100 hover:bg-rose-300 hover:shadow-xl hover:pointer hover:text-white transition'><MdContactSupport className='mr-1' /> Contato</Link>
+            </div>
+            <p className='text-xl text-left mt-6 ml-5'>Seu plano: <span className='text-pink-500 uppercase font-medium'>VIP</span></p>
+          </div>
         </div>
     </div>
   </div>
